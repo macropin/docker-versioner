0247f538d9d7eede01fd79199f22a48daa97d467
Andrew Cutler <macropin@gmail.com>
2016-10-04 15:56:47 +1100

    Show which mode operating

--- version.sh
+++ version.sh
@@ -13,17 +13,17 @@ if [ "$2" == "continuous" ]; then
     SHA256=$(echo "${PROJECT}" | sha256sum | awk '{ print $1 }')
     BUILDID=$(curl -s https://buildids.panubo.vgrd.net/id/${SHA256})
     VERSION="${GIT_VERSION}-${BUILDID}"
+    echo "Version Tag is: $VERSION (continuous)"
 else
     GIT_VERSION=$(git describe --always --tags --long)
     PROJECT=$(basename $(pwd))
     SHA256=$(echo "${PROJECT}${GIT_VERSION}" | sha256sum | awk '{ print $1 }')
     BUILDID=$(curl -s https://buildids.panubo.vgrd.net/id/${SHA256})
     VERSION="${GIT_VERSION}-${BUILDID}"
+    echo "Version Tag is: $VERSION (regular)"
 fi
 
 popd 1> /dev/null
-
-echo "Version Tag is: $VERSION"
 echo $VERSION > version/tag
 echo $BUILDID > version/buildid
 echo $PROJECT > version/project
